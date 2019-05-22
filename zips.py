8641e8130b76b99baf949b8c1c7ac5553baec9ed
Marc Lytle <mlytle4218@gmail.com>
2019-05-22 15:38:57 -0700

    trying to remove file

--- zips.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import csv
-import pandas as pd 
-import numpy as np 
-
-df = pd.read_csv('free-zipcode-database-Primary.csv')
-df_new = df.loc[df['State'] == 'CA']
-print(df_new.head(25))
